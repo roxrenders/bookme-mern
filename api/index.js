7ec3f02d5d70a7f522cba54466529d7140fd59e4
rox <raunaksahu55@gmail.com>
2023-08-19 16:54:46 +0530

    origin updated

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(cors({
     credentials: true,
-    origin: 'https://airbnb-clone-mern-r.vercel.app', // Update with your actual frontend domain
+    origin: 'https://airbnb-clone-mern-r.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization', 'token'],
 }));
