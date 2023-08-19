79f9d4afc319e93574e1af7d4548e2e901e2094b
rox <raunaksahu55@gmail.com>
2023-08-19 11:28:09 +0530

    L to Login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -40,7 +40,7 @@ try {
   return (
     <div className='mt-4 grow flex items-center justify-around'>
       <div className='mb-40'>
-        <h1 className='text-4xl text-center mb-4'>L</h1>
+        <h1 className='text-4xl text-center mb-4'>LogIn</h1>
         <form className='max-w-md mx-auto' onSubmit={handleLoginSubmit}>
           <input
             type="email"
