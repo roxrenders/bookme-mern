5ddb9919b4708054775894901ec549651a88f925
rox <raunaksahu55@gmail.com>
2023-08-19 11:25:43 +0530

    chnged LogIn to L for testing connection

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -40,7 +40,7 @@ try {
   return (
     <div className='mt-4 grow flex items-center justify-around'>
       <div className='mb-40'>
-        <h1 className='text-4xl text-center mb-4'>Login</h1>
+        <h1 className='text-4xl text-center mb-4'>L</h1>
         <form className='max-w-md mx-auto' onSubmit={handleLoginSubmit}>
           <input
             type="email"
