53df6c5b3b1abd1caaf78682345405df40321a88
rox <raunaksahu55@gmail.com>
2023-08-22 00:39:44 +0530

    E

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(cors({
-    
+    credentials: true,
     origin: 'https://airbnb-clone-mern-f.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization', 'token'],
