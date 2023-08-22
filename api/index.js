3e5a7d0fa1dda8d359c5ffec0361e5c4562c2c27
rox <raunaksahu55@gmail.com>
2023-08-22 19:34:42 +0530

    w

--- api/index.js
+++ api/index.js
@@ -34,8 +34,6 @@ app.use(cors({
 }));
 
 
-
-
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB");
