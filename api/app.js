5470d686218c279516d5ac338e7c9cb51a20fdf3
rox <raunaksahu55@gmail.com>
2023-08-23 02:52:42 +0530

    try

--- api/app.js
+++ api/app.js
@@ -34,6 +34,20 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'token'],
 }));
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+// Your code
+
 
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
