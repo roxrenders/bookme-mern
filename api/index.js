e38c7649eceb6d720bd2b87d452b847eb14aff17
rox <raunaksahu55@gmail.com>
2023-08-24 21:59:30 +0530

    updated origin with vercel server

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bookme-mern-front.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "token"],
   })
